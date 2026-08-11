5459687adb3ed069f8d91cf878cd541ed506ab9a
wizos <wizos@qq.com>
2026-08-12 02:48:20 +0800

    fix(Android): 忽略系统托管路由的清理提示

--- clash-lib/src/proxy/tun/routes/other.rs
+++ clash-lib/src/proxy/tun/routes/other.rs
@@ -5,14 +5,18 @@ use crate::{app::net::OutboundInterface, config::internal::config::TunConfig};
 
 #[allow(dead_code)]
 pub fn add_route(_: &OutboundInterface, _: &IpNet) -> std::io::Result<()> {
-    warn!("add_route is not implemented on {}", std::env::consts::OS);
+    if !cfg!(target_os = "android") {
+        warn!("add_route is not implemented on {}", std::env::consts::OS);
+    }
     Ok(())
 }
 
 pub fn maybe_routes_clean_up(_: &TunConfig) -> std::io::Result<()> {
-    warn!(
-        "maybe_routes_clean_up is not implemented on {}",
-        std::env::consts::OS
-    );
+    if !cfg!(target_os = "android") {
+        warn!(
+            "maybe_routes_clean_up is not implemented on {}",
+            std::env::consts::OS
+        );
+    }
     Ok(())
 }
